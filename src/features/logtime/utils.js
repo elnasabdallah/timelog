1d04e212dbe8ee671d397549299b8d11be7a2a21
mabdullahi <mabdullahi@aptly.de>
2023-12-13 16:07:01 +0100

    time bug fix

--- src/features/logtime/utils.js
+++ src/features/logtime/utils.js
@@ -42,7 +42,7 @@ export const formatDate = (date) => {
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
 
-  return `${day}.${month}.${year}`;
+  return `${year}-${month}-${day}`;
 };
 
 const convert = (date) => {
